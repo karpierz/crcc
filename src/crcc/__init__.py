85b4eb19eed3b2d2769199248b0ad86cdffebe70
Adam Karpierz <akarpierz@gmail.com>
2026-01-30 14:03:40 +0100

    Update documentation

--- src/crcc/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 1994 Adam Karpierz
-# SPDX-License-Identifier: Zlib
-
-"""Public Python API of CRC package."""
-
-from .__about__ import * ; del __about__  # type: ignore[name-defined]  # noqa
-
-from ._crc import * ; del _crc  # type: ignore[name-defined]  # noqa
